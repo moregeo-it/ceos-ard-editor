7a4ad903a6eca7f1e65c14369d56ab7fe2a75d79
Matthias Mohr <webmaster@mamo-net.de>
2026-01-05 18:11:28 +0100

    Implement splitter #30

---
+++ src/components/ide/EditorPane.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container fluid class="pa-4">
+    <slot name="message"></slot>
+    Editor Component
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'EditorPane',
+  computed: {
+    isArchived() {
+      // todo
+      return this.workspace?.status === 'archived'
+    },
+  },
+}
+</script>
---
+++ src/components/ide/PreviewPane.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container fluid class="pa-0"> Preview Component </v-container>
+</template>
+
+<script>
+export default {
+  name: 'PreviewPane',
+}
+</script>
